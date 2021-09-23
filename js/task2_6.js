21b88e8fc4e7bdd6fa0e4981766b0cc7ffe41683
Aves2001 <lebid2001@gmail.com>
2021-09-23 12:49:02 +0300

    fix task2_6

--- js/task2_6.js
+++ js/task2_6.js
@@ -1,12 +1,12 @@
 $(function(){
 //ЗАВДАННЯ 2.6
-	function prozor(name_obj, time, opacity)  {
+	function prozor(name_obj, time, opacity) {
 		var obj = $(name_obj);
-		var time_s = time * 1000;
-		$(obj).fadeTo(time_s, opacity);
-		$(obj).slideUp(time_s);
-		$(obj).slideDown(time_s);
-		$(obj).fadeTo(time_s, 1);
+		time *= 1000;
+		$(obj).fadeTo(time, opacity);
+		$(obj).slideUp(time);
+		$(obj).slideDown(time);
+		$(obj).fadeTo(time, 1);
     }
 	
 	prozor("#img_2", 2, 0.5);
